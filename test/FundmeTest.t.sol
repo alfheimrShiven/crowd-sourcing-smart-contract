f4164b384021bcb775c994b108a1704ee578e76f
Shivendra Singh <shiven@alfheimr.tech>
2023-07-01 23:59:06 +0530

    added script to deploy contract

---
+++ test/FundmeTest.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testCheckMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), address(this)); // the FundMeTest contract is deploying the FundMe.sol contract as it's a script
+    }
+}
